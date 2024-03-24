3307aa03e5bd3364385668fb96bec5971e77f999
Victor Ghita <tomi.gvf@gmail.com>
2024-03-24 22:18:54 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,17 @@ import { defineConfig, squooshImageService } from "astro/config";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   image: {
     service: squooshImageService()
   },
-  integrations: [tailwind(), sitemap(), robots(), react(), mdx()],
-  output: "server"
+  integrations: [tailwind(), sitemap(), react(), mdx()],
+  output: "server",
+  adapter: cloudflare()
 });
